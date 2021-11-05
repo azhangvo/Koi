36d7ed827f6edffa67b8b0b52a235019bb4e445f
kyleplo <31634240+kyleplo@users.noreply.github.com>
2021-11-05 07:53:51 -0400

    Fix issue with arguments
    As far as I can tell, Koi commands can't have are arbitrary amount of arguments, so the `-dice <min> <max>` command has been changed to `-dice <min>-<max>`, which is just a single argument.

--- src/fun/dice/DiceCommand.ts
+++ src/fun/dice/DiceCommand.ts
@@ -22,14 +22,14 @@ class DiceCommand extends Command {
 \`-dice\` - roll a 6-sided dice
 \`-dice <sides>\` - roll a dice with a specific amount of sides
 \`-dice <stuff>\` - choose randomly from a list of comma separated stuff
-\`-dice <min> <max>\` - choose randomly between numbers in a range
+\`-dice <min>-<max>\` - choose randomly between numbers in a range
 \`-dice help\` - show this help message`, Constants.black));
         } else if (args.length === 1 && !isNaN(parseFloat(args[0]))){
             var rand = Math.floor(Math.random() * parseFloat(args[0])) + 1;
             await msg.channel.send(createBaseEmbed("You rolled a " + rand, Constants.black));
-        } else if (args.length === 2 && !isNaN(parseFloat(args[0])) && !isNaN(parseFloat(args[1]))){
-            var a = parseFloat(args[0]);
-            var b = parseFloat(args[1]);
+        } else if (args.length === 1 && !isNaN(parseFloat(args[0].split("-")[0])) && !isNaN(parseFloat(args[0].split("-")[1]))){
+            var a = parseFloat(args[0].split("-")[0]);
+            var b = parseFloat(args[0].split("-")[1]);
             var min = Math.min(a, b);
             var max = Math.max(a, b);
             var rand = Math.ceil(Math.random() * (max - min + 1) + (min - 1))
@@ -46,7 +46,7 @@ class DiceCommand extends Command {
 \`-dice\` - roll a 6-sided dice
 \`-dice <sides>\` - roll a dice with a specific amount of sides
 \`-dice <stuff>\` - choose randomly from a list of comma separated stuff
-\`-dice <min> <max>\` - choose randomly between numbers in a range
+\`-dice <min>-<max>\` - choose randomly between numbers in a range
 \`-dice help\` - show this help message`, 0x89023e));
         }
     }
